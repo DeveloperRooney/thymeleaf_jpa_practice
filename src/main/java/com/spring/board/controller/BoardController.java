d5eb3cc9a22068e59cebf9910725bbb9528f9228
DeveloperRooney <han_0427@naver.com>
2021-07-28 13:36:31 +0900

    JPA를 이용한 페이징 처리

--- src/main/java/com/spring/board/controller/BoardController.java
+++ src/main/java/com/spring/board/controller/BoardController.java
@@ -1,15 +1,55 @@
 package com.spring.board.controller;
 
+import com.spring.board.domain.BoardVo;
+import com.spring.board.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
 
+    private BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
     @GetMapping("/board/list")
     public String boardList(Model model) {
 
+        model.addAttribute("boardList", boardService.boardList());
+
         return "board/boardList";
+    }
+
+    @GetMapping("/board/listPage")
+    public String boardList(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
+
+
+        model.addAttribute("boardList", boardService.getBoardList(pageNum));
+
+        return "board/boardList";
+    }
+
+    @GetMapping("/board/write")
+    public String boardWrite() {
+
+        return "board/boardWrite";
+    }
+
+    @PostMapping("/board/write")
+    public String boardWriteDo(BoardVo vo) {
+
+
+        System.out.println(vo.getAuthor());
+        System.out.println(vo.getTitle());
+        System.out.println(vo.getContent());
+
+        boardService.boardInsert(vo);
+
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/spring/board/service/BoardService.java
+++ src/main/java/com/spring/board/service/BoardService.java
@@ -3,8 +3,12 @@ package com.spring.board.service;
 import com.spring.board.domain.BoardVo;
 import com.spring.board.entity.Board;
 import com.spring.board.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +17,24 @@ public class BoardService {
 
     private BoardRepository boardRepository;
 
+    private static final int BlockPageCount = 5;
+    private static final int PageArticleCount = 10;
+
     public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
+    }
+
+    public void boardInsert(BoardVo vo) {
+        Board board = Board.builder()
+                        .id(vo.getId())
+                        .regDate(LocalDateTime.now())
+                        .updateDate(LocalDateTime.now())
+                        .author(vo.getAuthor())
+                        .title(vo.getTitle())
+                        .content(vo.getContent())
+                        .build();
+
+        boardRepository.save(board);
     }
 
     public List<BoardVo> boardList() {
@@ -31,6 +51,30 @@ public class BoardService {
                     .title(boardEntity.getTitle())
                     .content(boardEntity.getContent())
                     .build();
+            boardList.add(boardVo);
+        }
+
+        return boardList;
+    }
+
+    public List<BoardVo> getBoardList(Integer pageNum) {
+
+        Page<Board> page = boardRepository.findAll(PageRequest.of(
+                pageNum-1, PageArticleCount, Sort.by(Sort.Direction.DESC, "id")
+        ));
+
+        List<Board> boards = page.getContent();
+        List<BoardVo> boardList = new ArrayList<>();
+
+        for (Board board : boards) {
+            BoardVo boardVo = BoardVo.builder()
+                    .id(board.getId())
+                    .regDate(board.getRegDate())
+                    .updateDate(board.getUpdateDate())
+                    .author(board.getAuthor())
+                    .title(board.getTitle())
+                    .content(board.getContent()).build();
+
             boardList.add(boardVo);
         }
 
